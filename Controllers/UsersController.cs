35c994b844f1b67cff2e491983380ddffae27399
Ragul S <80128@SHSSDOM.COM>
2025-06-11 14:29:32 +0530

    Initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApi.Data;
+using MyApi.Models;
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserRepository _repo;
+
+        public UsersController(UserRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return _repo.GetAllUsers();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            _repo.AddUser(user);
+            return Ok(new { message = "User added successfully." });
+        }
+    }
+}
---
+++ Data/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using MyApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace MyApi.Data
+{
+    public class UserRepository
+    {
+        private readonly string _connectionString;
+
+        public UserRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("NextGenConString");
+        }
+
+        public List<User> GetAllUsers()
+        {
+            var users = new List<User>();
+
+            using SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT Id, Name, Email FROM Users", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                users.Add(new User
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    Email = reader["Email"].ToString()
+                });
+            }
+
+            return users;
+        }
+
+        public void AddUser(User user)
+        {
+            using SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "INSERT INTO Users (Name, Email) VALUES (@Name, @Email)", conn);
+
+            cmd.Parameters.AddWithValue("@Name", user.Name);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
+
