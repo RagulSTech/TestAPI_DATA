32672583e0786eb154ee5dbd08155fe58e668b08
RAGUL_SUBRAMANIAM <ragulmani097@gmail.com>
2025-06-17 22:44:21 +0530

    Update UsersController.cs

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,29 +1,58 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using MyApi.Models;
-using MyApi.Data;
+using Npgsql;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
 
 [ApiController]
 [Route("api/[controller]")]
 public class UsersController : ControllerBase
 {
-    private readonly UserRepository _userRepository;
+    private readonly string _connectionString;
 
-    public UsersController(UserRepository userRepository)
+    public UsersController(IConfiguration configuration)
     {
-        _userRepository = userRepository;
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
     }
 
     [HttpGet]
     public async Task<IActionResult> GetUsers()
     {
-        var users = await _userRepository.GetAllUsersAsync();
+        var users = new List<User>();
+
+        using var conn = new NpgsqlConnection(_connectionString);
+        await conn.OpenAsync();
+
+        using var cmd = new NpgsqlCommand("SELECT id, name, email FROM users", conn);
+        using var reader = await cmd.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            users.Add(new User
+            {
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                Email = reader.GetString(2)
+            });
+        }
+
         return Ok(users);
     }
 
     [HttpPost]
     public async Task<IActionResult> AddUser([FromBody] User user)
     {
-        var id = await _userRepository.AddUserAsync(user);
+        using var conn = new NpgsqlConnection(_connectionString);
+        await conn.OpenAsync();
+
+        using var cmd = new NpgsqlCommand("INSERT INTO users (name, email) VALUES (@name, @email) RETURNING id", conn);
+        cmd.Parameters.AddWithValue("name", user.Name);
+        cmd.Parameters.AddWithValue("email", user.Email);
+
+        var id = (int)await cmd.ExecuteScalarAsync();
+
         return Ok(new { id });
     }
 }
