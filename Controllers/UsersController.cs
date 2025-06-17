d2c79dad8b4f77f5b7116713580f9301808565c2
RAGUL_SUBRAMANIAM <ragulmani097@gmail.com>
2025-06-17 22:04:25 +0530

    Update UsersController.cs

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,39 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MyApi.Data;
-using MyApi.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace MyApi.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    private readonly UserRepository _repo;
+
+    public UsersController(UserRepository repo)
     {
-        private readonly UserRepository _repo;
+        _repo = repo;
+    }
 
-        public UsersController(UserRepository repo)
-        {
-            _repo = repo;
-        }
-        [HttpGet("get")]
-        public IActionResult MyGet() => Ok("Hello");
+    [HttpGet("get")]
+    public IActionResult MyGet() => Ok("Hello");
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-        {
-            var users = await _repo.GetAllUsersAsync();
-            return Ok(users);
-        }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        var users = await _repo.GetAllUsersAsync();
+        return Ok(users);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> AddUser(User user)
-        {
-            if (user == null)
-                return BadRequest("User data is required.");
+    [HttpPost]
+    public async Task<IActionResult> AddUser(User user)
+    {
+        if (user == null)
+            return BadRequest("User data is required.");
 
-            var id = await _repo.AddUserAsync(user);
-            return Ok(new { message = "User added successfully.", userId = id });
-        }
+        var id = await _repo.AddUserAsync(user);
+        return Ok(new { message = "User added successfully.", userId = id });
     }
 }
